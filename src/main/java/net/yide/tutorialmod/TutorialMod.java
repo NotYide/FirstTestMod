bbd6dee92277ebe3c44ae74926271f58caa56a01
NotYide <jace.sobrado10@gmail.com>
2025-04-24 15:39:17 +0400

    Added Custom Groups

--- src/main/java/net/yide/tutorialmod/TutorialMod.java
+++ src/main/java/net/yide/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.yide.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.yide.tutorialmod.item.ModBlocks;
+import net.yide.tutorialmod.item.ModItemGroups;
 import net.yide.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemsGroup();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
---
+++ src/main/java/net/yide/tutorialmod/item/ModItemGroups.java
@@ -0,0 +1,37 @@
+package net.yide.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.yide.tutorialmod.TutorialMod;
+
+public class ModItemGroups {
+	public static final ItemGroup CUSTOM_GROUP = Registry.register(Registries.ITEM_GROUP,
+			Identifier.of(TutorialMod.MOD_ID, "custom_group"),
+			FabricItemGroup.builder()
+					.icon(() -> new ItemStack(ModItems.CUSTOM_KATANA))
+					.displayName(Text.translatable("itemgroup.tutorialmod.custom_items"))
+					.entries((displayContext, entries) -> {
+		entries.add(ModItems.CUSTOM_CROWN);
+		entries.add(ModItems.CUSTOM_KATANA);
+	}).build());
+
+	public static final ItemGroup CUSTOM_BLOCK_GROUP = Registry.register(Registries.ITEM_GROUP,
+			Identifier.of(TutorialMod.MOD_ID, "custom_block_group"),
+			FabricItemGroup.builder()
+					.icon(() -> new ItemStack(ModBlocks.CUSTOM_ORE))
+					.displayName(Text.translatable("itemgroup.tutorialmod.custom_blocks"))
+					.entries((displayContext, entries) -> {
+						entries.add(ModBlocks.CUSTOM_ORE);
+					}).build());
+
+	public static void registerItemsGroup() {
+		TutorialMod.LOGGER.info("Registering Item Groups for" + TutorialMod.MOD_ID);
+	}
+}
